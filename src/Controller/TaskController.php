5f003383906bbb3eb2dba6361eb5c66142a12d3a
Julien Bisson <julien.bisson2@gmail.com>
2023-10-06 11:09:59 +0200

    commit task's controller and typeform

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Task;
+
+
+class TaskController extends AbstractController
+{
+
+    private $entityManager;
+
+    public function __construct($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/', name: 'task_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        $tasks = $this->entityManager->getRepository(Task::class)->findAll();
+
+        return $this->render('tasks/index.html.twig', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'task_show', methods: ['GET'])]
+
+    public function show(Task $task): Response
+    {
+        
+        return $this->render('tasks/show.html.twig', [
+            'task' => $task,
+        ]);
+    }
+    
+    #[Route('/task/new', name: 'task_new', methods: ['GET', 'POST'])]
+    public function newTask(Request $request): Response {
+        $task = new Tasks();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request); 
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->entityManager;
+            $entityManager->persist($task);
+            $entityManager->flush();
+    
+            return $this->redirectToRoute('tasks');
+        }
+    
+        return $this->render('tasks/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
---
+++ src/Form/TaskType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tasks;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('list')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tasks::class,
+        ]);
+    }
+
+    
+}
